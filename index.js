911db99c17266ca436192bc67a35517a43b75646
于基庆 <1942018083@qq.com>
2024-06-19 13:39:36 +0800

    feat: 增加配置项

--- index.js
+++ index.js
@@ -2,30 +2,38 @@ const {app, BrowserWindow, ipcMain} = require('electron');
 const path = require('node:path');
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        },
-    });
-    win.loadFile('index.html');
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      webSecurity: false, //是否禁用同源策略
+      webviewTag: true, // 启用 webview 标签
+      nodeIntegration: true, // 启用 Node Integration
+      contextIsolation: false, // 禁用上下文隔离
+      enableRemoteModule: true, // 启用远程模块
+      nodeIntegrationInSubFrames: true, // 启用 Node Integration in <iframe>
+    },
+  });
+  win.loadFile('index.html');
 };
 
-app.whenReady().then(() => {
-    ipcMain.handle('ping', () => 'pong');
-    createWindow();
+app.commandLine.appendSwitch("disable-site-isolation-trials");
 
-    app.on('activate', () => {
-        if(BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    })
+app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong');
+  createWindow();
+
+  app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  })
 });
 
 app.on('window-all-closed', () => {
-    if(process.platform !== 'darwin') {
-        app.quit();
-    }
+  if(process.platform !== 'darwin') {
+    app.quit();
+  }
 })
 
--- preload.js
+++ preload.js
@@ -1,8 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('versions', {
-    node: () => process.versions.node,
-    chrome: () => process.versions.chrome,
-    electron: () => process.versions.electron,
-    ping: () => ipcRenderer.invoke('ping')
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping'),
 })
