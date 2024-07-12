8ab7b77f7b8add7d024fbb80d3c9979ac9703c2b
于基庆 <1942018083@qq.com>
2024-07-12 09:03:01 +0800

    feat: 增加测试

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const {app, BrowserWindow, ipcMain} = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('node:path');
+
+app.commandLine.appendSwitch("disable-site-isolation-trials");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1366,
+    height: 768,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       webSecurity: false, //是否禁用同源策略
-      webviewTag: true, // 启用 webview 标签
-      nodeIntegration: true, // 启用 Node Integration
-      contextIsolation: false, // 禁用上下文隔离
-      enableRemoteModule: true, // 启用远程模块
-      nodeIntegrationInSubFrames: true, // 启用 Node Integration in <iframe>
+      // nodeIntegration: true, // 启用 Node Integration
+      // enableRemoteModule: true, // 启用远程模块
+      // nodeIntegrationInSubFrames: true, // 启用 Node Integration in <iframe>
     },
   });
   win.loadFile('index.html');
 };
-
-app.commandLine.appendSwitch("disable-site-isolation-trials");
 
 app.whenReady().then(() => {
   ipcMain.handle('ping', () => 'pong');
